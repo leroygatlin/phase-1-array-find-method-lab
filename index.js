6d134c78f29739e083380a1718d78a9e25a5c00d
leroygatlin <leroygatlin@gmail.com>
2023-11-13 15:53:50 -0800

    complete array find method

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// code your solution here   
+function superbowlWin(record){
+    const foundObject = record.find(element =>{
+    return element.result === "W"
+    })
+    if (foundObject){
+        return foundObject.year
+    } else { return undefined}
+}
